4f061893c9d7256ec6b6a3f811fbaee7bc272de6
sidd <supersid2001@gmail.com>
2022-03-01 23:13:31 -0500

    Changed page to functional component (NOT COMPLETE)

--- pages/editablePage.js
+++ pages/editablePage.js
@@ -1,5 +1,5 @@
-import React from 'react';
 import EditableBlock from '../lib/editableBlock';
+import React, {useEffect, useState} from 'react';
 
 const uid = () => {
   return Date.now().toString(36) + Math.random().toString(36).substr(2);
@@ -17,17 +17,15 @@ const setCaretToEnd = (element) => {
 
 const initialBlock = { id: uid(), html: "", tag: "p" };
 
-class EditablePage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.updatePageHandler = this.updatePageHandler.bind(this);
-    this.addBlockHandler = this.addBlockHandler.bind(this);
-    this.deleteBlockHandler = this.deleteBlockHandler.bind(this);
-    this.state = { blocks: [initialBlock]};
-  }
+function EditablePage() {
+  const[blockArr, setblockArr] = useState([initialBlock]);
 
-  updatePageHandler(updatedBlock) {
-    const blocks = this.state.blocks;
+  useEffect(() => {
+    console.log(blockArr);
+  }, [blockArr])
+
+  const updatePageHandler = updatedBlock => {
+    const blocks = blockArr;
     const index = blocks.map((b) => b.id).indexOf(updatedBlock.id);
     const updatedBlocks = [...blocks];
     updatedBlocks[index] = {
@@ -35,53 +33,51 @@ class EditablePage extends React.Component {
       tag: updatedBlock.tag,
       html: updatedBlock.html
     };
-    this.setState({ blocks: updatedBlocks });
+    setblockArr(updatedBlocks);
   }
 
-  addBlockHandler(currentBlock) {
+  const addBlockHandler = currentBlock => {
     const newBlock = { id: uid(), html: "", tag: "p" };
-    const blocks = this.state.blocks;
+    const blocks = blockArr;
     const index = blocks.map((b) => b.id).indexOf(currentBlock.id);
     const updatedBlocks = [...blocks];
     updatedBlocks.splice(index + 1, 0, newBlock);
-    this.setState({ blocks: updatedBlocks }, () => {
+    setblockArr(updatedBlocks, () => {
       currentBlock.ref.nextElementSibling.focus();
     });
   }
 
-  deleteBlockHandler(currentBlock) {
+  const deleteBlockHandler = currentBlock => {
     const previousBlock = currentBlock.ref.previousElementSibling;
     if (previousBlock) {
-      const blocks = this.state.blocks;
+      const blocks = blockArr;
       const index = blocks.map((b) => b.id).indexOf(currentBlock.id);
       const updatedBlocks = [...blocks];
       updatedBlocks.splice(index, 1);
-      this.setState({ blocks: updatedBlocks }, () => {
+      setblockArr(updatedBlocks, () => {
         setCaretToEnd(previousBlock);
         previousBlock.focus();
       });
     }
   }
 
-  render() {
-    return (
-      <div className="Page">
-        {this.state.blocks.map((block, key) => {
-          return (
-            <EditableBlock
-              key={key}
-              id={block.id}
-              tag={block.tag}
-              html={block.html}
-              updatePage={this.updatePageHandler}
-              addBlock={this.addBlockHandler}
-              deleteBlock={this.deleteBlockHandler}
-            />
-          );
-        })}
-      </div>
-    );
-  }
+  return (
+    <div className="Page">
+      {blockArr.map((block, key) => {
+        return (
+          <EditableBlock
+            key={key}
+            id={block.id}
+            tag={block.tag}
+            html={block.html}
+            updatePage={updatePageHandler.bind(this)}
+            addBlock={addBlockHandler.bind(this)}
+            deleteBlock={deleteBlockHandler.bind(this)}
+          />
+        );
+      })}
+    </div>
+  );
 }
 
 
