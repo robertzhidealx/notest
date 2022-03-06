50bd85e6e4ad530176b291e93d1bee992e7c4847
sidd <supersid2001@gmail.com>
2022-03-06 12:37:08 -0500

    Minor changes

--- pages/MyNotes/index.js
+++ pages/MyNotes/index.js
@@ -2,8 +2,16 @@ import { useEffect, useState } from 'react';
 import { noteService } from "../../services/note.services";
 import { useRouter } from 'next/router'
 
+const uid = () => {
+  return Date.now().toString(36) + Math.random().toString(36).substr(2);
+};
+
+const initialBlock = { id: uid(), html: 'Start here', tag: 'p' };
+
 const MyNotes = () =>{
   const [notes, setNotes] = useState([]);
+  const [name, setName] = useState('');
+  const [author, setAuthor] = useState('');
   const router = useRouter()
   useEffect(() => {
     (async () => {
@@ -16,9 +24,35 @@ const MyNotes = () =>{
     router.push('/MyNotes/' + noteId)
   }
 
+  const handleSubmit = () =>{
+    const date = Date.now()/1000;
+    (async () => {
+      await noteService.create(name, author, [initialBlock], date, []);
+    })().then(router.reload(window.location.pathname));
+  }
+
+
   return (
     <div>
       <h1>Your Notes</h1>
+      <form onSubmit={handleSubmit}>
+      Name:
+      <input
+        name='name'
+        type='text'
+        onChange={e => setName(e.target.value)}
+      />
+      Author:
+      <input
+        name='author'
+        type='text'
+        onChange={e => setAuthor(e.target.value)}
+      />
+      <button>
+        Add Note
+      </button>
+      </form>
+
       {notes.map((note, key) => {
           return (
             <button onClick={() => openNote(note._id)}>
