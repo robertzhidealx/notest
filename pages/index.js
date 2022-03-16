53f6f7e3842dbff7455c11ac805338044edc710b
Robert Zhang <jiaxuanzhang066@gmail.com>
2022-03-15 21:58:14 -0400

    feat: add homepage redirect

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,19 @@
+import { useRouter } from 'next/router';
+
 const Home = () => {
-  return <>Welcome to Notest!</>;
+  const router = useRouter();
+
+  return (
+    <>
+      <h1>Welcome to Notest!</h1>
+      <button
+        className='border bg-cyan-200'
+        onClick={() => router.push('/note')}
+      >
+        Go to notes!
+      </button>
+    </>
+  );
 };
 
 export default Home;
